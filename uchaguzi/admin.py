7d78b4d83fbd7d880f8e64006908a955da5b14c1
Ribiro <deno059861>
2022-11-24 17:49:44 +0300

    created abstract user

--- uchaguzi/admin.py
+++ uchaguzi/admin.py
@@ -1,7 +1,11 @@
 from django.contrib import admin
-from .models import PollingStation, Candidate, Result
+from .models import PollingStation, Candidate, POResult, CustomUser
 
 # Register your models here.
+class CustomUserAdmin(admin.ModelAdmin):
+    list_display = ("username","first_name","last_name","email","id_no", "assigned_polling_station")
+    
+admin.site.register(CustomUser, CustomUserAdmin)
 admin.site.register(PollingStation)
 admin.site.register(Candidate)
-admin.site.register(Result)
+admin.site.register(POResult)
